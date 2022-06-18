9a0efe53d1eb43e528c8acd5079939043be397ef
TiagoPiccinatto <tiagoaff2030@gmail.com>
2022-06-18 10:57:21 -0300

    alteracao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { SobreComponent } from './componentes/sobre/sobre.component';
+import { NavComponent } from './componentes/nav/nav.component';
+import { ContatoComponent } from './componentes/contato/contato.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    SobreComponent,
+    NavComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
